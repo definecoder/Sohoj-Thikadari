757cfb62b5fa2209163fb590ab42d69002181674
codermehraj <codermehraj@gmail.com>
2023-11-03 08:21:05 +0600

    added home page layout

--- Frontend/src/pages/HomePage/HomePage.jsx
+++ Frontend/src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,8 @@
 import { useLocation } from "react-router-dom";
 import NavBar from "../../components/navBar/NavBar";
 
+import "./HomePage.css";
+
 export default function HomePage() {
 
     // getting uid from login or signup :v
@@ -8,7 +10,19 @@ export default function HomePage() {
 
     return <>
         <NavBar />
-        <h1>hello</h1>
+        <div className="home-canvas">
+            
+            <div className="home-news-canvas"></div>
+            <div className="home-right-canvas">
+                <div className="home-profile-canvas"></div>    
+                <div className="home-content-canvas">
+                    <div className="home-recent-program-canvas"></div>
+                    <div className="home-user-status-canvas"></div>
+                </div>
+
+            </div>            
+
+        </div>
     </>;
 
 }
