2a9b1a41c2bfc5c102b7d0896d26458f82adb8ae
codermehraj <codermehraj@gmail.com>
2023-11-03 07:57:43 +0600

    finished design of navbar

--- Frontend/src/components/navBar/NavBar.jsx
+++ Frontend/src/components/navBar/NavBar.jsx
@@ -1,15 +1,19 @@
 import "./NavBar.css";
 import navLogo from "../../assets/appLogo.png";
 
+import HomeIcon from '@mui/icons-material/Home';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import LogoutIcon from '@mui/icons-material/Logout';
+
 export default function NavBar() {
 
     return <>
         <div className="navCanvas">
             <div className="navLogo"><img src={navLogo} alt="" /></div>
             <div className="navRightContainer">
-                <div className="navElement"> ড্যাশবোর্ড </div>
-                <div className="navElement"> ফার্মসমূহ </div>
-                <div className="navButton"> লগআউট </div>
+                <div className="navElement"> <HomeIcon fontSize="large"/> <span className="dashboadNavText">ড্যাশবোর্ড</span></div>
+                <div className="navElement"> <FormatListBulletedIcon fontSize="large"/> <span className="firmsNavText">ফার্মসমূহ</span> </div>
+                <div className="navButton"> <LogoutIcon fontSize="large"/> <span className="logoutNavText">লগআউট</span> </div>
             </div>
         </div>
     </>;
