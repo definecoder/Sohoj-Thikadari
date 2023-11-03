b032fd83cbb7b5ac63e19d574506185cf277bb63
codermehraj <codermehraj@gmail.com>
2023-11-03 23:43:20 +0600

    added homepage news section and did css layouting of other sections

---
+++ Frontend/src/pages/HomePage/newsData.js
@@ -0,0 +1,77 @@
+const newsData = [
+    {
+        newsText: "২৫-০২-২০২২ খ্রি. হতে ১১-০৩-২০২৩ খ্রি. পর্যন্ত গমের সূচি, উপ-সূচি ও মেয়াদ দেখতে ক্লিক করূন",
+        newsLink: "https://dgfood.portal.gov.bd/site/files/95f9be06-034c-4bee-83e4-8f766e7bd0e6",
+        newsId: 1,
+    },
+    {
+        newsText: "২৫-০২-২০২২ খ্রি. হতে ১১-০৩-২০২৩ খ্রি. পর্যন্ত গমের সূচি, উপ-সূচি ও মেয়াদ দেখতে ক্লিক করূন",
+        newsLink: "https://dgfood.portal.gov.bd/site/files/95f9be06-034c-4bee-83e4-8f766e7bd0e6",
+        newsId: 2
+    },
+    {
+        newsText: "২৫-০২-২০২২ খ্রি. হতে ১১-০৩-২০২৩ খ্রি. পর্যন্ত গমের সূচি, উপ-সূচি ও মেয়াদ দেখতে ক্লিক করূন",
+        newsLink: "https://dgfood.portal.gov.bd/site/files/95f9be06-034c-4bee-83e4-8f766e7bd0e6",
+        newsId: 3
+    },
+    {
+        newsText: "২৫-০২-২০২২ খ্রি. হতে ১১-০৩-২০২৩ খ্রি. পর্যন্ত গমের সূচি, উপ-সূচি ও মেয়াদ দেখতে ক্লিক করূন",
+        newsLink: "https://dgfood.portal.gov.bd/site/files/95f9be06-034c-4bee-83e4-8f766e7bd0e6",
+        newsId: 4
+    },
+    {
+        newsText: "২৫-০২-২০২২ খ্রি. হতে ১১-০৩-২০২৩ খ্রি. পর্যন্ত গমের সূচি, উপ-সূচি ও মেয়াদ দেখতে ক্লিক করূন",
+        newsLink: "https://dgfood.portal.gov.bd/site/files/95f9be06-034c-4bee-83e4-8f766e7bd0e6",
+        newsId: 5
+    },
+    {
+        newsText: "২৫-০২-২০২২ খ্রি. হতে ১১-০৩-২০২৩ খ্রি. পর্যন্ত গমের সূচি, উপ-সূচি ও মেয়াদ দেখতে ক্লিক করূন",
+        newsLink: "https://dgfood.portal.gov.bd/site/files/95f9be06-034c-4bee-83e4-8f766e7bd0e6",
+        newsId: 6
+    },
+    {
+        newsText: "২৫-০২-২০২২ খ্রি. হতে ১১-০৩-২০২৩ খ্রি. পর্যন্ত গমের সূচি, উপ-সূচি ও মেয়াদ দেখতে ক্লিক করূন",
+        newsLink: "https://dgfood.portal.gov.bd/site/files/95f9be06-034c-4bee-83e4-8f766e7bd0e6",
+        newsId: 7
+    },
+    {
+        newsText: "২৫-০২-২০২২ খ্রি. হতে ১১-০৩-২০২৩ খ্রি. পর্যন্ত গমের সূচি, উপ-সূচি ও মেয়াদ দেখতে ক্লিক করূন",
+        newsLink: "https://dgfood.portal.gov.bd/site/files/95f9be06-034c-4bee-83e4-8f766e7bd0e6",
+        newsId: 8
+    },
+    {
+        newsText: "২৫-০২-২০২২ খ্রি. হতে ১১-০৩-২০২৩ খ্রি. পর্যন্ত গমের সূচি, উপ-সূচি ও মেয়াদ দেখতে ক্লিক করূন",
+        newsLink: "https://dgfood.portal.gov.bd/site/files/95f9be06-034c-4bee-83e4-8f766e7bd0e6",
+        newsId: 9
+    },
+    {
+        newsText: "২৫-০২-২০২২ খ্রি. হতে ১১-০৩-২০২৩ খ্রি. পর্যন্ত গমের সূচি, উপ-সূচি ও মেয়াদ দেখতে ক্লিক করূন",
+        newsLink: "https://dgfood.portal.gov.bd/site/files/95f9be06-034c-4bee-83e4-8f766e7bd0e6",
+        newsId: 10
+    },
+    {
+        newsText: "২৫-০২-২০২২ খ্রি. হতে ১১-০৩-২০২৩ খ্রি. পর্যন্ত গমের সূচি, উপ-সূচি ও মেয়াদ দেখতে ক্লিক করূন",
+        newsLink: "https://dgfood.portal.gov.bd/site/files/95f9be06-034c-4bee-83e4-8f766e7bd0e6",
+        newsId: 11
+    },
+    {
+        newsText: "২৫-০২-২০২২ খ্রি. হতে ১১-০৩-২০২৩ খ্রি. পর্যন্ত গমের সূচি, উপ-সূচি ও মেয়াদ দেখতে ক্লিক করূন",
+        newsLink: "https://dgfood.portal.gov.bd/site/files/95f9be06-034c-4bee-83e4-8f766e7bd0e6",
+        newsId: 12
+    },
+    {
+        newsText: "২৫-০২-২০২২ খ্রি. হতে ১১-০৩-২০২৩ খ্রি. পর্যন্ত গমের সূচি, উপ-সূচি ও মেয়াদ দেখতে ক্লিক করূন",
+        newsLink: "https://dgfood.portal.gov.bd/site/files/95f9be06-034c-4bee-83e4-8f766e7bd0e6",
+        newsId: 13
+    },
+    {
+        newsText: "২৫-০২-২০২২ খ্রি. হতে ১১-০৩-২০২৩ খ্রি. পর্যন্ত গমের সূচি, উপ-সূচি ও মেয়াদ দেখতে ক্লিক করূন",
+        newsLink: "https://dgfood.portal.gov.bd/site/files/95f9be06-034c-4bee-83e4-8f766e7bd0e6",
+        newsId: 14
+    },
+
+    
+
+];
+
+export default newsData;
