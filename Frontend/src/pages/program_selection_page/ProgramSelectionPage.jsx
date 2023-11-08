91bddff4607967e48b57bfc879e2b1e15ebeb354
codermehraj <codermehraj@gmail.com>
2023-11-09 04:07:24 +0600

    connected api of fetching the invoices that are sent only

--- Frontend/src/pages/program_selection_page/ProgramSelectionPage.jsx
+++ Frontend/src/pages/program_selection_page/ProgramSelectionPage.jsx
@@ -3,23 +3,50 @@ import NavBar from "../../components/navBar/NavBar";
 import RecentProgramInfoCard from "../../components/RecentProgramInfoCard/RecentProgramInfoCard";
 import programList from "./ProgramList";
 import { Link } from "react-router-dom";
-export default function FirmProfilePage() {
+import {useEffect, useState} from 'react';
+import { Navigate, useNavigate, useParams } from 'react-router-dom'
+import axios from 'axios';
+
+export default function FirmProfilePage() {  
+
+  let { firmId } = useParams();
+
+  const [sendingPrograms, setSendingPrograms] = useState([...programList]);    
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:8888/api/v1/invoice/sending/" + firmId,
+          {
+            headers: { Authorization: localStorage.getItem('token') },
+          }
+        );
+        // console.log(localStorage.getItem('token'));
+        //console.log(res.data.Invoice);
+        setSendingPrograms(res.data.Invoice);    
+        //console.log(sendingPrograms);
+      } catch (error) {}
+    };
+    fetchData();    
+  }, []);
+
   return (
     <>
       <NavBar />
       <div className="ps-main-wrapper">
         <div className="ps-header">প্রোগ্রাম নির্বাচন করুন</div>
         <div className="ps-grid-wrapper">
-          {programList.map((program) => {
+          {sendingPrograms.map((program) => {
             return (
-              <div className="ps-program" key={program.programId}>
+              <div className="ps-program" key={program.invoiceNo}>
                 {/* invoice no pathate hobe */}
                 
                 <RecentProgramInfoCard
                   // className="ps-program"
                   {...program}
-                  route={"/firm/"+program.programId+"/addRecievingInfo"}
-                  key={program.programId}
+                  route={"/firm/"+program.invoiceNo+"/addRecievingInfo"}
+                  key={program.invoiceNo}
                 />
               </div>
             );
