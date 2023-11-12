d302873123019e3e827d6f377bea93f276468e09
shawon-majid <shawon.majid@gmail.com>
2023-11-12 18:40:15 +0600

    govt bill api created

--- backend/routes/bills.js
+++ backend/routes/bills.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const { createBill, getBill, getAllBills } = require('../controllers/bills')
+const { createBill, getBill, getAllBills, getBillsGov, updateBillsGov } = require('../controllers/bills')
 
 
 router.route('/').post(createBill)
 router.route('/:billId').get(getBill)
 router.route('/all/:firmId').get(getAllBills)
+router.route('/govtBills/:firmId').get(getBillsGov).put(updateBillsGov)
 
 module.exports = router;
