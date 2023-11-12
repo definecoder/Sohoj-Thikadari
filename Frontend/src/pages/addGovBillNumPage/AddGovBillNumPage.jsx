a2fc6e352e44044991e490538712470c30e838a8
shawon-majid <shawon.majid@gmail.com>
2023-11-13 01:22:03 +0600

    add gov bill page api integrated

--- Frontend/src/pages/addGovBillNumPage/AddGovBillNumPage.jsx
+++ Frontend/src/pages/addGovBillNumPage/AddGovBillNumPage.jsx
@@ -2,34 +2,38 @@ import NavBar from "../../components/navBar/NavBar";
 import "./AddGovBillNumPage.css";
 import AddGovBillCard from "../../components/addGovBillCard/AddGovBillCard";
 
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function AddGovBillNumPage() {
-  const pendingGovBills = [
-    {
-      billNo: "চাউল / ২৬ / ২০২১-২২২৩",
-      amount: "২৪২০৫৬",
-      billDate: "১০/০৩/২০২৩",
-    },
-    {
-      billNo: "চাউল / ২৭ / ২০২১-২২২৩",
-      amount: "২৪২০৫৬",
-      billDate: "১০/০৩/২০২৩",
-    },
-    {
-      billNo: "চাউল / ২৬ / ২০২১-২২২৩",
-      amount: "২৪২০৫৬",
-      billDate: "১০/০৩/২০২৩",
-    },
-    {
-      billNo: "চাউল / ২৬ / ২০২১-২২২৩",
-      amount: "২৪২০৫৬",
-      billDate: "১০/০৩/২০২৩",
-    },
-    {
-      billNo: "চাউল / ২৬ / ২০২১-২২২৩",
-      amount: "২৪২০৫৬",
-      billDate: "১০/০৩/২০২৩",
-    },
-  ];
+  var done = false;
+  let { firmId } = useParams();
+
+  const [pendingGovBills, setPendingGovBills] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!done)
+        try {
+          done = true;
+          const response = await axios.get(
+            "http://localhost:8888/api/v1/bills/govtBills/" + firmId,
+            {
+              headers: { Authorization: localStorage.getItem("token") },
+            }
+          );
+
+          setPendingGovBills(response.data);
+
+          // setInvoiceCount(res.data.)
+        } catch (error) {
+          alert(error.response.msg);
+          done = false;
+        }
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
@@ -37,16 +41,24 @@ export default function AddGovBillNumPage() {
       <div className="gov-bill-num-add-page-canvas">
         <div className="add-gov-bill-num-page-title">সরকারি বিল নম্বর দিন</div>
         <div className="main-content-gov-bill-add">
-          {pendingGovBills.map((bill, indx) => {
-            return (
-              <AddGovBillCard
-                billNo={bill.billNo}
-                amount={bill.amount}
-                billDate={bill.billDate}
-                key={indx}
-              />
-            );
-          })}
+          {pendingGovBills.length !== 0 ? (
+            pendingGovBills.map((bill, indx) => {
+              return (
+                <AddGovBillCard
+                  billNo={bill.billNo}
+                  amount={bill.amount}
+                  billDate={bill.billDate}
+                  key={indx}
+                />
+              );
+            })
+          ) : (
+            <>
+              <h1>
+                <i>সরকারি নাম্বার দেওয়ার মত বিল নেই</i>
+              </h1>
+            </>
+          )}
         </div>
       </div>
     </>
