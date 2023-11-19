db4d610c82144d3df4ba5fbc702f36000a4dbd90
shawon-majid <shawon.majid@gmail.com>
2023-11-19 09:53:42 +0600

    changed host and port setting

--- backend/app.js
+++ backend/app.js
@@ -40,6 +40,8 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server is running on port ${port}`);
 });
